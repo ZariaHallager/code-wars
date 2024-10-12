29bc9daa7c14b63e6fb8fcc3f7ca7bad49596810
Sagespellman <sagespellman@outlook.com>
2024-10-12 00:44:19 -0400

    day3: Add main.js

---
+++ day3/main.js
@@ -0,0 +1,7 @@
+function stringToArray(str) {
+    return str.split(' ');
+}
+
+// Examples:
+console.log(stringToArray("Robin Singh")); // ["Robin", "Singh"]
+console.log(stringToArray("I love arrays they are my favorite")); // ["I", "love", "arrays", "they", "are", "my", "favorite"]
