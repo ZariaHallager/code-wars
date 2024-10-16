0dacdbf7e097420726da46487fdd1703d0788c52
Sagespellman <sagespellman@outlook.com>
2024-10-15 22:40:31 -0400

    adding three days worth of missing solutions

---
+++ day6/main.js
@@ -0,0 +1,7 @@
+function numberToString(num) {
+    return num.toString();
+}
+
+console.log(numberToString(123));  // "123"
+console.log(numberToString(999));  // "999"
+console.log(numberToString(-100)); // "-100"
---
+++ day7/main.js
@@ -0,0 +1,8 @@
+const stringToNumber = function (str) {
+    return Number(str);
+};
+
+console.log(stringToNumber("1234")); // Output: 1234
+console.log(stringToNumber("605"));  // Output: 605
+console.log(stringToNumber("1405")); // Output: 1405
+console.log(stringToNumber("-7"));   // Output: -7
