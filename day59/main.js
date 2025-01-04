ab3910d147ab1d40dfe97f5891d098a2e3b72395
Zaria Hallager <120356303+ZariaHallager@users.noreply.github.com>
2025-01-03 21:10:40 -0500

    day 59 codewars

---
+++ day59/main.js
@@ -0,0 +1,12 @@
+// You received an array with two strings.
+// Create a function that will return their product as a string.
+// E.g.
+
+// input: ['3', '5'] => output: '15'
+// input: ['2', '-3'] => output: '-6'
+// input: ['9', '0'] => output: '0'
+
+function arrMultiply(arr) {
+    return String(arr[0] * arr[1]);
+
+}
